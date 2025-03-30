3d507c7b024f60f1de8b593586be6c1e4aea5695
arjittiwari <50325904+arjittiwari@users.noreply.github.com>
2025-03-30 09:41:05 +0530

    Time: 715 ms (0%), Space: 9.6 MB (0%) - LeetHub

--- 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.c
+++ 1534-minimum-number-of-frogs-croaking/1534-minimum-number-of-frogs-croaking.c
@@ -4,7 +4,6 @@
 int frogCount[MAX_FROG];
 int count =0,freed=0;
 bool checkSound(int sound){
-    // printf("\n sound is %d with count %d",sound,count);
     if (sound==0) {
         if (freed == 0){
             count++;
@@ -12,27 +11,22 @@ bool checkSound(int sound){
             freed--;
         }
         frogCount[sound]++;
-        // printf("\ninc %d with count %d and freed %d",sound, count, freed);
         return(true);
     } else if (sound == MAX_SOUND -1) {
         if (frogCount[sound-1]<=0) {
-            printf("\nfail1");
             return(false);
         } else {
             frogCount[sound-1]--;
-            // printf("\nfrog sound at %d is %d with count %d",sound,frogCount[sound],count);
             freed++;
             return(true);
         }
     }
     if (frogCount[sound-1]<=0) {
-        printf("\nfail2 sound at %d is %d",sound-1,frogCount[sound-1]);
         return(false);
     } else {
         frogCount[sound-1]--;
         frogCount[sound]++;
     }
-    // printf("\nfrog sound at %d is %d",sound,frogCount[sound]);
     return(true);
 }
 int minNumberOfFrogs(char* croakOfFrogs) {
@@ -40,12 +34,6 @@ int minNumberOfFrogs(char* croakOfFrogs) {
     count=0;
     freed=0;
     memset(frogCount,0,sizeof(int)*MAX_FROG);
-    // i=0;
-    // while (i<MAX_FROG){
-    //     printf(" %d",frogCount[i]);
-    //     i++;
-    // }
-    // i=0;
     while (i < strlen(croakOfFrogs)) {
         switch (croakOfFrogs[i]) {
             case 'c':
@@ -66,21 +54,11 @@ int minNumberOfFrogs(char* croakOfFrogs) {
             case 'default':
                 return(FAILURE);
         }
-        // printf("\ncount check %d",count);
         if (!checkSound(val)) {
-            // printf("\n%d",val);
             return(FAILURE);
         }
         i++;
     }
-    printf("\nreached here");
-    printf("\n");
-    i=0;
-    while (i<MAX_FROG){
-        // printf(" %d",frogCount[i]);
-        i++;
-    }
-    printf("\n");
     i=0;
     val=0;
     while (i < MAX_FROG) {
